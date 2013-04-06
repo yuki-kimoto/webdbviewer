9e4d16851d59f5e0314d6395cd1ca6b5c4038a57
Yuki Kimoto <kimoto.yuki@gmail.com>
2013-04-06 20:45:57 +0900

    updated DBViewer plugin

---
+++ mojo/lib/Mojolicious/Plugin/AutoRoute.pm
@@ -0,0 +1,113 @@
+package Mojolicious::Plugin::AutoRoute;
+use Mojo::Base 'Mojolicious::Plugin';
+
+our $VERSION = '0.06';
+
+sub register {
+  my ($self, $app, $conf) = @_;
+  
+  # Renderer
+  my $renderer = $app->renderer;
+  
+  # Parent route
+  my $r = $conf->{route} || $app->routes;
+  
+  # Top directory
+  my $top_dir = $conf->{top_dir} || 'auto';
+  $top_dir =~ s#^/##;
+  $top_dir =~ s#/$##;
+  
+  # Index
+  $r->route('/')->to(cb => sub { shift->render("/$top_dir/index") });
+  
+  # Route
+  $r->route('/(*anything_path)')->to(cb => sub {
+    my $c = shift;
+    
+    my $path = $c->stash('anything_path');
+    $path = 'index' unless defined $path;
+    
+    if ($path =~ /\.\./) {
+      $c->render_exception('Forbidden');
+      return;
+    }
+    
+    my $found;
+    for my $dir (@{$app->renderer->paths}) {
+      if (-f "$dir/$top_dir/$path.html.ep") {
+        $found = 1;
+        last;
+      }
+    }
+    
+    if ($found) { $c->render("/$top_dir/$path") }
+    else { $c->render_not_found }
+  });
+}
+
+1;
+
+=head1 NAME
+
+Mojolicious::Plugin::AutoRoute - Mojolicious Plugin to create routes automatically
+
+=head1 CAUTION
+
+B<This is beta release. Implementation will be changed without warnings>. 
+
+=head1 SYNOPSIS
+
+  # Mojolicious
+  $self->plugin('AutoRoute');
+
+  # Mojolicious::Lite
+  plugin 'AutoRoute';
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::AutoRoute> is a L<Mojolicious> plugin
+to create routes automatically.
+
+Routes corresponding to URL is created .
+
+  TEMPLATES                           ROUTES
+  templates/auto/index.html.ep        # /
+                /foo.html.ep          # /foo
+                /foo/bar.html.ep      # /foo/bar
+                /foo/bar/baz.html.ep  # /foo/bar/baz
+
+If you like C<PHP>, this plugin is very good.
+You only put file into C<auto> directory.
+
+=head1 OPTIONS
+
+=head2 C<route>
+
+  route => $route;
+
+You can set parent route if you need.
+This is L<Mojolicious::Routes> object.
+Default is C<$app->routes>.
+
+=head2 C<top_dir>
+
+  top_dir => 'myauto'
+
+Top directory. default is C<auto>.
+
+=head1 METHODS
+
+L<Mojolicious::Plugin::AutoRoute> inherits all methods from
+L<Mojolicious::Plugin> and implements the following new ones.
+
+=head2 C<register>
+
+  $plugin->register($app);
+
+Register plugin in L<Mojolicious> application.
+
+=head1 SEE ALSO
+
+L<Mojolicious>, L<Mojolicious::Guides>, L<http://mojolicio.us>.
+
+=cut
--- mojo/lib/Mojolicious/Plugin/DBViewer.pm
+++ mojo/lib/Mojolicious/Plugin/DBViewer.pm
@@ -7,7 +7,7 @@ use DBIx::Custom;
 use Validator::Custom;
 use Carp 'croak';
 
-our $VERSION = '0.12';
+our $VERSION = '0.13';
 
 has 'command';
 has 'prefix';
@@ -89,24 +89,8 @@ sub register {
     my $footer_link = $conf->{footer_link}
       || 'http://search.cpan.org/dist/Mojolicious-Plugin-DBViewer'
        . '/lib/Mojolicious/Plugin/DBViewer.pm';
-    
-    my $r = $r->route("/$prefix")->to(
-      'dbviewer#',
-      namespace => $namespace,
-      plugin => $self,
-      sprefix => $sprefix,
-      site_title => $site_title,
-      driver => $driver,
-      dbviewer => $self,
-      default_charset => $default_charset,
-      footer_text => $footer_text,
-      footer_link => $footer_link
-    );
-    
-    # Default
-    $r->get('/')->to('#default');
-    
-    # Tables
+
+    # Utilities
     my $utilities = [
       {path => 'create-tables', title => 'Create tables'},
       {path => 'primary-keys', title => 'Primary keys'},
@@ -120,32 +104,23 @@ sub register {
     push @$utilities,
       {path => 'select-statements', title => 'Selects statements'};
     
-    # Tables
-    $r->get('/tables')->to('#tables', utilities => $utilities);
+    # Route Config
+    my $r = $r->route("/$prefix")->to(
+      'dbviewer#',
+      namespace => $namespace,
+      plugin => $self,
+      sprefix => $sprefix,
+      site_title => $site_title,
+      driver => $driver,
+      dbviewer => $self,
+      default_charset => $default_charset,
+      footer_text => $footer_text,
+      footer_link => $footer_link,
+      utilities => $utilities
+    );
     
-    # Table
-    $r->get('/table')->to('#table');
-    
-    # Show create tables
-    $r->get('/create-tables')->to('#create_tables');
-    
-    # Show select tables
-    $r->get('/select-statements')->to('#select_statements');
-    
-    # Show primary keys
-    $r->get('/primary-keys')->to('#primary_keys');
-    
-    # Show null allowed columns
-    $r->get('/null-allowed-columns')->to('#null_allowed_columns');
-    
-    # Select
-    $r->get('/select')->to('#select');
-    
-    # MySQL Only
-    if ($driver eq 'mysql') {
-      $r->get('/database-engines')->to('#database_engines');
-      $r->get('/charsets')->to('#charsets');
-    }
+    # Auto Route
+    $app->plugin('AutoRoute', route => $r, top_dir => 'dbviewer/auto');
   }
 }
 
@@ -181,11 +156,11 @@ B<This module is alpha release. features will be changed without warnings.>
   http://localhost:3000/dbviewer
   
   # Prefix change (http://localhost:3000/dbviewer2)
-  plugin 'DBViewer', dbh => $dbh, prefix => 'dbviewer2';
+  plugin 'DBViewer', dsn => $dsn, prefix => 'dbviewer2';
 
   # Route
   my $bridge = $app->route->under(sub {...});
-  plugin 'DBViewer', route => $bridge, ...;
+  plugin 'DBViewer', dsn => $dsn, route => $bridge;
 
 =head1 DESCRIPTION
 
@@ -287,7 +262,7 @@ Router, default to C<$app->routes>.
 It is useful when C<under> is used.
 
   my $bridge = $r->under(sub {...});
-  plugin 'DBViewer', dbh => $dbh, route => $bridge;
+  plugin 'DBViewer', dsn => $dsn, route => $bridge;
 
 =head2 user
 
